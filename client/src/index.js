005a820251ccab47cf94a2095c9459c7ade51a09
Shreyas <shreyxs@gmail.com>
2021-06-08 20:33:51 +0530

    linking frontend and backend repositories

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
-import React from "react";
-import Routes from "./Routes";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
+import Loading from "react-fullscreen-loading";
+import registerServiceWorker from "./registerServiceWorker";
+const Routes = lazy(() => import("./Routes"));
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(
+	<Suspense
+		fallback={<Loading loading background="#000" loaderColor="#3498db" />}
+	>
+		<Routes />
+	</Suspense>,
+	document.getElementById("root")
+);
+
+registerServiceWorker();
